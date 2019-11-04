11a83acd0d7088f4045c859cb2b6a24effc9b5e5
abhisek11 <abhishek.singh@shyamfuture.com>
2019-11-04 12:12:24 +0530

    secret  addedn

---
+++ secrets.py
@@ -0,0 +1,18 @@
+#this file contains sensitive information like django secret key
+#edit this according to your requirement
+
+class Django_Secrets:
+    def __init__(self):
+        self.key = ''
+
+
+class Oauth_Secrets:
+    def __init__(self):
+        self.consumer_key = "AMskkMrTiil0Re1iKuJcQHseH"
+        self.consumer_secret = "fZ0nOB4IqqYka7P70EAVkHkRFeFjTYXC1dIuUaWn3gF8YiCp9a"
+        self.access_token = "3135870607-MXL8UNUuZEWcgHE6wdN2kTYpqumiQWC5FVNpnUR"
+        self.access_token_secret = "qMtb33qKZeGyMxqYJVICOjTYNuqZUOyhIlNBIlyrJWmOW"
+
+
+
+        
